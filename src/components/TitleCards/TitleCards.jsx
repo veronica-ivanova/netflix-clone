518de018d853b0c224a90f7d5a0bc1269f264bd9
veronica-ivanova <veronica.ivanova27@gmail.com>
2025-10-16 23:46:12 +0300

    feat: create Home page

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,9 +1,37 @@
 import './TitleCards.css'
+import cards_data from '../../assets/cards/Cards_data.js'
+import {useEffect, useRef} from "react";
 
-const TitleCards = () => {
+const TitleCards = ({title, category}) => {
+
+    const cardsRef = useRef(null);
+
+    const handleWheel = (event) => {
+        event.preventDefault();
+        if (cardsRef.current) {
+            cardsRef.current.scrollLeft += event.deltaY;
+        }
+    }
+
+    useEffect(() => {
+        const cards = cardsRef.current;
+        if (!cards) return;
+
+        cards.addEventListener('wheel', handleWheel);
+        return () => cards.removeEventListener('wheel', handleWheel);
+    },[])
+
     return (
-        <div className="titleCards">
-
+        <div className="title-сards">
+            <h2>{title || "Popular on Netflix"}</h2>
+            <div className="card-list" ref={cardsRef}>
+                {cards_data.map((card, index) => {
+                    return <div className="card" key={index}>
+                        <img src={card.image} alt=""/>
+                        <p>{card.name}</p>
+                    </div>
+                })}
+            </div>
         </div>
     )
 }
