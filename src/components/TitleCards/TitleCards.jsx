715911a993e7b4f67bb2114c83f199fd057d7c25
veronica-ivanova <veronica.ivanova27@gmail.com>
2025-10-19 19:32:49 +0300

    feat: fetch and display movie information from TMDB API

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,10 +1,40 @@
 import './TitleCards.css'
 import cards_data from '../../assets/cards/Cards_data.js'
-import {useEffect, useRef} from "react";
+import {useEffect, useRef, useState} from "react";
 
 const TitleCards = ({title, category}) => {
 
+    const [apiData, setApiData] = useState([]);
     const cardsRef = useRef(null);
+
+    // const options = {
+    //     method: 'GET',
+    //     headers: {
+    //         accept: 'application/json',
+    //         Authorization: `Bearer ${import.meta.env.VITE_TMDB_TOKEN}`
+    //     }
+    // };
+    const fetchData = async () => {
+        try {
+            const res = await fetch(
+                `https://api.themoviedb.org/3/movie/${category?category:"now_playing"}?language=en-US&page=1`,
+                {
+                    method: 'GET',
+                    headers: {
+                        accept: 'application/json',
+                        Authorization: `Bearer ${import.meta.env.VITE_TMDB_TOKEN}`
+                    },
+                }
+            )
+            if (!res.ok) {
+                throw new Error(`Ошибка ${res.status} ${res.statusText}`);
+            }
+            const data = await res.json();
+            setApiData(data.results);
+        } catch (error) {
+            console.log("Ошибка загрузки API", error)
+        }
+    }
 
     const handleWheel = (event) => {
         event.preventDefault();
@@ -14,6 +44,12 @@ const TitleCards = ({title, category}) => {
     }
 
     useEffect(() => {
+        // fetch(`https://api.themoviedb.org/3/movie/${category?category:"now_playing"}?language=en-US&page=1`, options)
+        //     .then(res => res.json())
+        //     .then(res => setApiData(res.results))
+        //     .catch(err => console.error(err));
+        fetchData();
+
         const cards = cardsRef.current;
         if (!cards) return;
 
@@ -25,10 +61,10 @@ const TitleCards = ({title, category}) => {
         <div className="title-сards">
             <h2>{title || "Popular on Netflix"}</h2>
             <div className="card-list" ref={cardsRef}>
-                {cards_data.map((card, index) => {
+                {apiData.map((card, index) => {
                     return <div className="card" key={index}>
-                        <img src={card.image} alt=""/>
-                        <p>{card.name}</p>
+                        <img src={`https://image.tmdb.org/t/p/w500` + card.backdrop_path} alt=""/>
+                        <p>{card.original_title}</p>
                     </div>
                 })}
             </div>
